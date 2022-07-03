911323ff9b8056ee21881a643ccba49f9f056233
Anisa <An1sa_jon@mail.ru>
2022-07-04 01:01:28 +0400

    Task_4

---
+++ Task_4/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+Console.WriteLine("Imput number");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num < 0) {
+    num = num * -1;
+}
+else if (num == 0) {
+    Console.WriteLine("The number is 0");  
+}
+
+int count = 0;
+
+while (count <= num)
+{
+    if ((count > 0) && (count % 2 == 0))
+    {
+        Console.Write(count + "  ");
+        count++;
+    }
+    else
+    {
+        count++;
+    }
+}
